22961bfdc0f64676ccd49ab4bf994591db44711d
Dineshreddy2007 <kanchetidineshreddy@gmail.com>
2026-08-27 11:34:47 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,8 @@
+x=int(input("Enter x:"))
+y=int(input("Enter y:"))
+print(x+y)
+
+l=int(input("Enter length:"))
+b=int(input("Enter breth:"))
+
+
---
+++ day2.py
@@ -0,0 +1,63 @@
+
+# print(6+3)
+# print(6-3)
+# print(6*3)
+# print(6/3)
+# print(-6//3)
+# print(6%3)
+# print(6**3)
+
+# print("c"+"d")
+# print("c"*8)
+
+
+#comparison operators
+#< > <= >= == !=
+#print(3>6)
+#print(8>4)
+#print("c">"d")
+#print("Dinesh">"Surya")
+
+
+#Lonical operator
+#and or not
+
+# x = 6
+# y = 8
+# print(x<7 and y>5)
+# print(y>5 or x>9)
+# print(y>12 and z<6)
+# print(not x>4)
+# print(not x)
+# z=0
+# print(not z)
+# print(x>3 or y<5)
+
+#Assignment operators
+# = += -= *= /= //= **= %=
+# x=5
+# x+=3 x= x+3
+# print(x)
+# x*=2
+# x/=4
+# print(x)
+
+# n=36
+# n%=4  n=n%4
+# print(n)
+
+# s="aa"
+# s*=2
+# print(s)
+
+s="h"
+s+="e"
+s+="l"
+s+="o"
+print(s)
+
+#walrus operator
+# x=5
+# print(x>3)
+
+# print(x:=5>3)
